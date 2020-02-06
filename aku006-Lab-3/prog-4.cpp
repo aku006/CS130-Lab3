16c41d29f39cd6663fee2b11532b23d8ad184b11
Alexander Ku <ax.ku195@gmail.com>
2020-02-06 12:32:46 -0800

    Submitting Lab 3

---
+++ aku006-Lab-3/prog-4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+void print_cases(const std::string& name)
+{
+    char up[name.size() + 1];
+    char lo[name.size() + 1];
+
+    for(size_t i = 0; i < name.size(); i++)
+    {
+        up[i] = std::toupper(name[i]);
+        lo[i] = std::tolower(name[i]);
+    }
+    up[name.size()] = '\0';
+    lo[name.size()] = '\0';
+
+    std::cout << lo << " " << up << std::endl;
+}
+
+int main()
+{
+    print_cases("Elizabeth");
+    print_cases("John");
+    print_cases("Frank");
+    print_cases("Fred");
+
+    return 0;
+}
+
